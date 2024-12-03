1b0b67c4975de35b2e95acb5f15b0a52a13976d7
Zipkyn <v.t.s.782003@gmail.com>
2024-12-03 22:19:07 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,20 @@
+import telebot
+from telebot.types import Message
+
+TOKEN = ''
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message: Message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    bot.send_message(message.chat.id, 'Привет! Я бот помогающий твоему здоровью.')
+
+@bot.message_handler(func=lambda message: True)
+def all_messages(message: Message):
+    print('Введите команду /start, чтобы начать общение.')
+    bot.send_message(message.chat.id, 'Введите команду /start, чтобы начать общение.')
+
+print("Bot is running...")
+bot.infinity_polling()
+
+
